f9bf9ae7e48ace210a7c2328293351aaa4c0f4ed
wtricks <webtricks.ak@gmail.com>
2023-04-10 21:37:18 +0530

    Made class A, B and implemented method override

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,5 @@
+public class A {
+    public void meth() {
+        return "Invoking method from class A";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        String result = new B().meth();
+
+        System.out.println(result);
+    }
 }
