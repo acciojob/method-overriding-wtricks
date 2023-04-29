2bbab2eafb96cce926258fd738ec85b7196a93fb
wtricks <webtricks.ak@gmail.com>
2023-04-29 08:32:47 +0530

    Hi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,11 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b = new B();
-        
-        System.out.println(b.meth());   
+           B b = new B();
+
+        System.out.println(b.meth());
+
+        System.out.println(b.meth());
     }
 }
 
